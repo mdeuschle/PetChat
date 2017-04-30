614e043f365030c3c3db46a7ca162eabfa37ac7a
Matt Deuschle <mdeuschle@gmail.com>
2017-04-30 11:30:13 -0700

    added Apple's camera library

---
+++ PetChat/CameraVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PetChat
+//
+//  Created by Matt Deuschle on 4/30/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
